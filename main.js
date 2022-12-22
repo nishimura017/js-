361086d3b999c64cbfc250f94c41118c443bb361
西村玲奈 <nishimurareina@nishimurareinanoMacBook-Pro.local>
2022-12-22 15:14:10 +0900

    演算子について質問です

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ display = document.getElementById('display');
 
 var tenFlg="false"; 
 
+  
 document.addEventListener('click',function(event) {
 
     //イコール押下時計算
@@ -17,13 +18,10 @@ document.addEventListener('click',function(event) {
         document.getElementById('display').innerText = '';
         return;
     }
+
+   
     
-    //数字を押下時ディスプレイに連続表示
-    if(!event.target.matches('.number'))return;
-    console.log(event.target.innerText);
-    let clickNumber = event.target.innerText;
-    let beforeClickNumber = display.innerText;
-    display.innerText = beforeClickNumber + clickNumber;
+
     
     //頭に０を連続押下時の制御
     if(((event.target.matches('.zero'))&& display.innerText=="0")||(display.innerText=="0"&&(event.target.matches('.dot')))) {
@@ -31,9 +29,22 @@ document.addEventListener('click',function(event) {
     }
 
     //演算子を連続で打てないよう制御
-    let result =display.innerText.slice(-1);
-    if((event.target.matches('.ope'))&& ( result == '.ope')) {
-        let doubleClick = display.innerText.replace(result,clickNumber);
-        display.innerText = doubleClick;
-    }
+        if(((event.target.matches('.ope')) && (display.innerText.slice (-1) == '.ope'))){return;}
+        else if(event.target.matches('.ope')){
+            console.log(event.target.innerText);   
+            let clickNumber = event.target.innerText;
+            let beforeClickNumber = display.innerText;
+            display.innerText = beforeClickNumber + clickNumber;
+            //display.innerText =display.innerText.replace(display.innerText.slice.call(-1,0),clickNumber);
+        }
+    
+    //数字を押下時ディスプレイに連続表示
+
+    if(event.target.matches('.number')) {
+        console.log(event.target.innerText);
+        let clickNumber = event.target.innerText;
+        let beforeClickNumber = display.innerText;
+        display.innerText = beforeClickNumber + clickNumber;
+        return;
+        }
 }); 
