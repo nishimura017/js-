83f25f994eca770b18d7e469336988aa0be7ef33
西村玲奈 <nishimurareina@nishimurareinanoMacBook-Pro.local>
2022-12-22 17:57:30 +0900

    電卓課題できました。

--- main.js
+++ main.js
@@ -2,49 +2,52 @@
 number = document.getElementById('number');
 display = document.getElementById('display');
 
-var tenFlg="false"; 
-
   
 document.addEventListener('click',function(event) {
 
-    //イコール押下時計算
-    if(event.target.matches('#eq')) {
-        const calcResut = eval(display.innerText);
-        display.innerText = calcResut;
+  //イコール押下時計算
+  if(event.target.matches('#eq')) {
+      const calcResut = eval(display.innerText);
+       display.innerText = calcResut;
+  }
+
+  //AC押下時
+  if(event.target.matches('.clear')) {
+      document.getElementById('display').innerText = '';
+      return;
+  }
+  
+  //頭に０を連続押下時
+  if(((event.target.matches('.zero'))&& display.innerText=="0")||(display.innerText=="."&&(event.target.matches('.zero')))) {
+      display.innerText = '0.';
+  }
+	
+
+
+  //演算子を連続で打てないよう制御
+  if(((event.target.matches('.ope')) && (display.innerText.slice(-1)=== "+"))){return;
+  }else if(((event.target.matches('.ope')) && (display.innerText.slice(-1)=== "-"))){return;
+  }else if(((event.target.matches('.ope')) && (display.innerText.slice(-1)=== "*"))){return;
+  }else if(((event.target.matches('.ope')) && (display.innerText.slice(-1)=== "/"))){return;
+  }else if(((event.target.matches('.ope')) && (display.innerText.slice(-1)=== "."))){return;
+  }else if(event.target.matches('.ope')){
+    console.log(event.target.innerText);   
+    let clickNumber = event.target.innerText;
+    let beforeClickNumber = display.innerText;
+    display.innerText = beforeClickNumber + clickNumber;
+  }
+
+	//数字押下時先頭が０なら消して表示
+  if((event.target.matches('.number'))&&(display.innerText == "0")) {
+          display.innerText = number;
+  }
+
+  //数字を押下時ディスプレイに連続表示
+  if(event.target.matches('.number')){
+    console.log(event.target.innerText);
+    let clickNumber = event.target.innerText;
+    let beforeClickNumber = display.innerText;
+    display.innerText = beforeClickNumber + clickNumber;
+    return;
     }
-
-    //AC押下時
-    if(event.target.matches('.clear')) {
-        document.getElementById('display').innerText = '';
-        return;
-    }
-
-   
-    
-
-    
-    //頭に０を連続押下時の制御
-    if(((event.target.matches('.zero'))&& display.innerText=="0")||(display.innerText=="0"&&(event.target.matches('.dot')))) {
-        display.innerText = '0.';
-    }
-
-    //演算子を連続で打てないよう制御
-        if(((event.target.matches('.ope')) && (display.innerText.slice (-1) == '.ope'))){return;}
-        else if(event.target.matches('.ope')){
-            console.log(event.target.innerText);   
-            let clickNumber = event.target.innerText;
-            let beforeClickNumber = display.innerText;
-            display.innerText = beforeClickNumber + clickNumber;
-            //display.innerText =display.innerText.replace(display.innerText.slice.call(-1,0),clickNumber);
-        }
-    
-    //数字を押下時ディスプレイに連続表示
-
-    if(event.target.matches('.number')) {
-        console.log(event.target.innerText);
-        let clickNumber = event.target.innerText;
-        let beforeClickNumber = display.innerText;
-        display.innerText = beforeClickNumber + clickNumber;
-        return;
-        }
 }); 
